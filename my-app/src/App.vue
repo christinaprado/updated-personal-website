9c3e595c6202adca485ccd3e204870506dafdfb6
christinaprado <158535437+christinaprado@users.noreply.github.com>
2025-02-12 11:59:41 +0000

    added comments

---
+++ my-app/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
+
+const comment = ref([])
+
+async function getComment() {
+  const { data } = await supabase.from('comment').select()
+  comment.value = data
+}
+
+onMounted(() => {
+  getComment()
+})
+</script>
+
+<template>
+<h1>Comments</h1>
+  <ul>
+    <li v-for="comment in comment" :key="comment.id">{{ comment.name }} {{ comment.comment }}
+    </li>
+  </ul>
+<h2>comments</h2>
+</template>
